331a5495a10b376db6036c6d356a90746e623b5d
Victor Vidal <victorvidal08@hotmail.com>
2022-02-02 20:17:12 -0300

    exercicios do dia

---
+++ modulo-1-fundamentos/java-script/dia-1-java-script/exercicio2.js
@@ -0,0 +1,9 @@
+const a = 45;
+const b = 66;
+
+if (a>b) {
+    console.log("a é maior que b")
+}
+else {
+    console.log("b é maior que a")
+}
--- modulo-1-fundamentos/java-script/dia-1-java-script/scripts.js
+++
@@ -1,7 +0,0 @@
-let age = 20;
-age = 30;
-
-let name = "Hulk";
-
-console.log(name);
-console.log(age);
