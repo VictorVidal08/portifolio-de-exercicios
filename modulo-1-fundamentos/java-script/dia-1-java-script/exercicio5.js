9f12d231de5c4fdbd7923c8fd2ec3405eb78b48f
Victor Vidal <victorvidal08@hotmail.com>
2022-02-02 22:14:32 -0300

    adiciona até exercicio 6 concluido

---
+++ modulo-1-fundamentos/java-script/dia-1-java-script/exercicio5.js
@@ -0,0 +1,15 @@
+const angulo1 = 60
+const angulo2 = 60
+const angulo3 = 60
+
+if (angulo1+angulo2+angulo3 === 180) {
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+
+
+let allAnglesArePositives = angulo1 > 0 && angulo2 > 0 && angulo3 > 0
+console.log(allAnglesArePositives)
