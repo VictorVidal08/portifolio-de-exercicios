e83febea777541d597c5d66b1c2f2caa9cdf13c5
Victor Vidal <victorvidal08@hotmail.com>
2022-02-03 20:29:02 -0300

    parcial dos exercicios do dia

---
+++ modulo-1-fundamentos/java-script/dia-2-java-script-array-e-loop-for/exercicio4.js
@@ -0,0 +1,23 @@
+//Com o mesmo código do exercício anterior, 
+//caso o valor final seja maior que 20, 
+//imprima a mensagem: "valor maior que 20". 
+//Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let result = 0;
+
+for (index = 0; index < numbers.length; index += 1) {
+    result = result + numbers[index];
+}
+
+let average =  result/ (numbers.length-1);
+
+console.log(average);
+
+if (average > 20) {
+    console.log("valor maior que 20");
+}
+else {
+    console.log("valor menor ou igual a 20");
+}
---
+++ modulo-1-fundamentos/java-script/dia-2-java-script-array-e-loop-for/exercicio5.js
@@ -0,0 +1,15 @@
+//Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let higher = 0;
+
+for (index = 1; index < numbers.length; index += 1) {
+    if (numbers[index] > higher) {
+        higher = numbers[index];
+    }
+    else {
+        higher = higher;
+    }
+}
+console.log(higher);
