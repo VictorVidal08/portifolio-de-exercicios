1883ac56e97bd710f335ce1a3df9ac648c39ce69
Victor Vidal <victorvidal08@hotmail.com>
2022-02-07 22:52:40 -0300

    inicia exercicios dia 4.4

---
+++ modulo-1-fundamentos/java-script/dia-4-java-script-objects-funcions/parte1-ex3.js
@@ -0,0 +1,6 @@
+// Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+
+//personagem
+//origem
+//nota
+//recorrente
---
+++ modulo-1-fundamentos/java-script/dia-4-java-script-objects-funcions/parte1-ex7.js
@@ -0,0 +1,9 @@
+//Adicione um novo livro favorito na chave livrosFavoritos , 
+//que é um array . Atribua a esta 
+//chave um objeto contendo as seguintes informações:
+
+{
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  }
