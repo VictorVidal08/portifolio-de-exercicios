7c100a9a65d6f6eca8586ea5c531010bf1953f26
Victor Vidal <victorvidal08@hotmail.com>
2022-03-03 21:30:56 -0300

    adiciona bonus parte 1

--- modulo-1-fundamentos/bloco-7-JS6-testesunitarios/dia-1-js-es6-let-const-arrow-functions-e-template-literals/parte-1-ex2.js
+++ modulo-1-fundamentos/bloco-7-JS6-testesunitarios/dia-1-js-es6-let-const-arrow-functions-e-template-literals/parte-1-ex2.js
@@ -1,7 +1,6 @@
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
-// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
 
 
 const sortOddsAndEvens = () => {
@@ -17,4 +16,15 @@ const sortOddsAndEvens = () => {
 
 const sortedArray = sortOddsAndEvens();
 
+console.log(`Os numeros ${sortedArray} se encontram ordenados de forma crescente!`); 
+
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+
+// https://www.w3schools.com/js/js_array_sort.asp
+
+const sortArrayBonus = array => {
+    const sortOddsAndEvens = array.sort((a, b) => a - b);
+    return sortOddsAndEvens;
+}
+const sortedArrayBonus = sortArrayBonus(oddsAndEvens);
 console.log(`Os numeros ${sortedArray} se encontram ordenados de forma crescente!`); 
