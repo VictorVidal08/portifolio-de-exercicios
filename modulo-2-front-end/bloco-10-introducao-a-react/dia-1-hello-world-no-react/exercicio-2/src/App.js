cfdeb3bea1ee23faafa268957f8052e9b94e07bb
Victor Vidal <victorvidal08@hotmail.com>
2022-04-04 22:02:45 -0300

    finaliza exercicios do dia 10.1

---
+++ modulo-2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-2/src/App.js
@@ -0,0 +1,17 @@
+import Header from './Header';
+// import Content from './Content';
+import './App.css';
+import Footer from './Footer';
+import Content from './Content';
+
+function App() {
+  return (
+   <div>
+     <Header />
+     <Content />
+     <Footer />
+   </div>
+  );
+}
+
+export default App;
