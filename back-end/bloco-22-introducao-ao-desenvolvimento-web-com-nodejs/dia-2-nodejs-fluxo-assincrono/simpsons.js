48a2808b30a9026d8762e9b53d89059347a896ca
VictorVidal08 <victorvidal08@hotmail.com>
2022-08-09 16:06:05 -0300

    finishing daily exercises

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/simpsons.js
@@ -0,0 +1,14 @@
+Vendo chevette marajó 1988. IPVA 2022 pago.
+
+Carro com suspensão preparada para drift (dorikich - canal sucataspec do youtube). Pode ser usado para rua e para 'brincadeiras' automotivas. Ideal para quem está começando no drift ou quer algum projeto já com suspensão boa!!.
+
+carro vai com:
+- suspensão (1500 reais)
+- farois e setas novas (400 reais)
+- bateria nova (300 reais)
+- velas, cabos de vela, óleo recém trocado, filtros novos.
+- diferencial aberto
+
+Está bonita de longe, de perto tem alguns detalhes. Não é veiculo para colecionador nem nada do tipo, mas para quem quer se divertir.
+
+
